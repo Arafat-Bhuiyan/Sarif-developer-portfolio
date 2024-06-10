ecfe92f3f70f658d36af8795ca77a3074108665d
Arafat-Bhuiyan <arafatbhuiyan41@gmail.com>
2024-06-10 15:52:50 +0600

    Building Home Page done

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,51 @@
+import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png'
-
+import './index.scss'
+import { useEffect, useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
 const Home = () => {
-    
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['a', 'r', 'i', 'f']
+    const jobArray = [
+        'w',
+        'e',
+        'b',
+        ' ',
+        'd',
+        'e',
+        'v',
+        'e',
+        'l',
+        'o',
+        'p',
+        'e',
+        'r',
+        '.'
+    ]
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 2000);
+    }, [])
+
     return (
         <div className="Container home-page">
             <div className="text-zone">
-                <h1>Hi, <br/> I'm
-                <img src={LogoTitle} alt="developer" />
-                arif
-                <br/>
-                web developer
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>
+                    <br />
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
+                    <img src={LogoTitle} alt="developer" />
+                    <AnimatedLetters letterClass={letterClass}
+                        strArray={nameArray}
+                        idx={15} />
+                    <br />
+                    <AnimatedLetters letterClass={letterClass}
+                        strArray={jobArray}
+                        idx={22} />
                 </h1>
                 <h2>Frontend Developer / JavaScript Expert</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
